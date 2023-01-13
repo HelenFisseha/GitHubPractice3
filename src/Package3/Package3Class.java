46ecdffd39cca0768ba755fb7b2774832ce7a8dd
HelenFisseha <121978538+HelenFisseha@users.noreply.github.com>
2023-01-13 10:07:06 -0500

    Update Package3Class.java
    added print statments

--- src/Package3/Package3Class.java
+++ src/Package3/Package3Class.java
@@ -3,5 +3,7 @@ package Package3;
 public class Package3Class {
 public static void main(String[] args) {
 	System.out.println("github practice 3");
+	System.out.println("github practice");
+	System.out.println("github ");
 }
 }
